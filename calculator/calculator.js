d40e67f5c4d755f0a511d27872f7d652b57652e4
pabloppizarro <pizarro.pablo@outlook.com>
2023-06-04 23:36:12 -0300

    init files for calculator project

---
+++ calculator/calculator.js
@@ -0,0 +1,11 @@
+console.log("Hellos ");
+function onOperatorClick(event) {
+    console.log(event.target);
+}
+function plus() {}
+function sum() {
+    const number = Number.parseInt("");
+}
+function rest() {}
+function multiply() {}
+function divide() {}
