5eb1c7f4ab2e3ea765f3ba205e22cc20878c7f12
pabloppizarro <pizarro.pablo@outlook.com>
2023-06-05 19:06:11 -0300

    basic calculator logic done

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,34 +1,114 @@
 const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
 const backButton = document.querySelector(".back");
+const resetButton = document.querySelector(".reset");
 const screenValue = document.querySelector(".screen-value");
+const resultButton = document.querySelector(".result");
+let stateA = [];
+let stateB = [];
+let lastOperator = "";
 
+onReset();
 bindButtons();
 
+function onReset() {
+    stateA = [];
+    stateB = [];
+    lastOperator = "";
+}
+
 function bindButtons() {
-    numberButtons.forEach((b) => {
-        b.addEventListener("click", (e) => {
-            console.log(e.target.innerText);
-            console.log(typeof toNumber(e.target.innerText));
-            screenValue.innerText = e.target.innerText;
+    numberButtons.forEach((numberButton) => {
+        numberButton.addEventListener("click", (e) => {
+            const numString = e.target.innerText;
+            // console.log(numString);
+            // console.log(typeof toNumber(numString));
+            setState(numString);
         });
     });
+    operatorButtons.forEach((operatorButton) => {
+        operatorButton.addEventListener("click", (event) => {
+            onOperatorChanged(event.target.innerText);
+        });
+    });
+    resetButton.addEventListener("click", () => {
+        onReset();
+        render();
+    });
+    backButton.addEventListener("click", () => onBack());
+    resultButton.addEventListener("click", () =>
+        doMath(lastOperator, stateA, stateB)
+    );
 }
 
-function onOperatorClick(event) {
-    console.log(event.target);
+function onBack() {
+    let actualState = getStateArray();
+    if (actualState.length > 0) {
+        actualState = actualState.pop();
+    }
+    render(toStringState(getStateArray()));
 }
-function plus() {}
-function sum() {
-    const number = Number.parseInt("");
-}
-function rest() {}
-function multiply() {}
-function divide() {}
 
-function toString(number) {
-    return number.toString();
+function render(newState = "0") {
+    screenValue.innerText = newState;
 }
-function toNumber(string) {
-    return parseInt(string);
+function setState(newState) {
+    getStateArray().push(newState);
+    const stringNumbers = getStateArray().join("");
+    render(stringNumbers);
+}
+
+function getStateArray() {
+    return lastOperator === "" ? stateA : stateB;
+}
+
+function sum(numA, numB) {
+    return numA + numB;
+}
+function rest(numA, numB) {
+    return numA - numB;
+}
+function multiply(numA, numB) {
+    return numA * numB;
+}
+function divide(numA, numB) {
+    return numA / numB;
+}
+
+function toStringState(state) {
+    return state.toString();
+}
+function toNumberState(state) {
+    return state.length > 0 ? parseInt(state.join("")) : 0;
+}
+
+function onOperatorChanged(operator) {
+    // console.log(`Operator clicked ! -> ${operator}`);
+    lastOperator = operator;
+}
+
+function doMath(operator, stateA, stateB) {
+    if (operator === "") return;
+    let result;
+    console.log(` lets do ${stateA} ${operator} ${stateB}`);
+    const numA = toNumberState(stateA);
+    const numB = toNumberState(stateB);
+    switch (operator) {
+        case "+":
+            result = sum(numA, numB);
+            break;
+        case "÷":
+            result = divide(numA, numB);
+            break;
+        case "×":
+            result = multiply(numA, numB);
+            break;
+        case "-":
+            result = rest(numA, numB);
+            break;
+        default:
+            break;
+    }
+    render(toStringState(result));
+    onReset();
 }
