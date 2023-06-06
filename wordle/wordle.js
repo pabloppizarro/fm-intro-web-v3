3fb069c8d8d3633f17ecb9308bd9167b997cf215
pabloppizarro <pizarro.pablo@outlook.com>
2023-06-06 15:58:01 -0300

    final html and css

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -1,1 +1,16 @@
 console.log("Hi Wordle People");
+
+const GET_API_URL = "https://words.dev-apis.com/word-of-the-day";
+const POST_API_URL = "https://words.dev-apis.com/validate-word";
+function isLetter(letter) {
+    return /^[a-zA-Z]$/.test(letter);
+}
+
+// document
+//     .querySelector(".tester-input")
+//     .addEventListener("keydown", function (event) {
+//         // uses the isLetter function from above
+//         if (!isLetter(event.key)) {
+//             event.preventDefault();
+//         }
+//     });
